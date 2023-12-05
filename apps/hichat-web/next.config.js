1993672c1d4ec849c0857500c914a2b4e55d1c7a
Phong Lee <phonglee1210@gmail.com>
2023-12-05 14:19:47 +0700

    Initial commit

--- apps/hichat-web/next.config.js
+++ apps/hichat-web/next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
-};
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+}
